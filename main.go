6d02bcfb498201c8843b4bfe324d56dcdc6c6ae8
almukhanbetov <almuko.m@gmail.com>
2026-01-30 20:36:22 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(`
+			<!doctype html>
+			<html lang="ru">
+			<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<title>Привет</title>
+			</head>
+			<body style="font-family: system-ui; padding: 40px;">
+			<h1>Hello GO</h1>
+			</body>
+			</html>
+		`))
+	})
+
+	r.Run(":8080")
+}
